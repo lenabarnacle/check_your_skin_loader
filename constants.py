c02b76643c6ee59d1b05d6c29e8affe18d0a5bea
lenabarnacle <epindurina@gmail.com>
2021-10-20 11:12:05 +1000

    refactor_2

---
+++ constants.py
@@ -0,0 +1,12 @@
+IN_SEP = '\t'
+IN_AUTH = ('test', '123')
+IN_GOOD_STATUS = (200, )
+SCHEME_IN = [
+    'unix_timestamp',
+    'datetime',
+    'email',
+    'skin_type',
+    'questions_data',
+    'answers_data',
+    'results_data_prod',
+    'results_data_test']
--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from dataimporters.data_importer import tests_results_importer
 import logging
-# import os
+import click
+import os
 
-# load_dotenv()
-# connection_string = os.getenv('PY_DWH_CONNECTION_STRING')
-# log_path = os.getenv('LOGGING_PATH') + 'check_your_skin_loader.log'
-
-# connection_string = 'postgresql://dwloader:QWEasd123@rc1b-bojd020c96w0gyci.mdb.yandexcloud.net:6432/dwh'
-connection_string = 'postgresql://elena:<j,hlj,h1@rc1b-bojd020c96w0gyci.mdb.yandexcloud.net:6432/dwh'
-log_path = r'C:\Users\79146\PycharmProjects\check_your_skin_loader\check_your_skin_loader.log'
+load_dotenv()
+connection_string = os.getenv('PY_DWH_CONNECTION_STRING')
+log_path = os.getenv('LOGGING_PATH') + 'check_your_skin_loader.log'
 
 FORMAT = '%(asctime)-15s %(name)s %(levelname)s %(message)s'
-logging.basicConfig(format=FORMAT, filename=log_path,level=logging.INFO)
-logger = logging.getLogger('urbn.loader.ss')
+logging.basicConfig(format=FORMAT, filename=log_path, level=logging.INFO)
+logger = logging.getLogger('urbn.loader.check_your_skin')
 
-# def check_your_skin_loader():
-logger.info('Start Check_your_skin_tests_results import')
-importer = tests_results_importer()
-importer.connect(connection_string)
-importer.get_tests_results_final()
-importer.save_tests_results()
-importer.disconnect()
-logger.info('End Check_your_skin_tests_results import')
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def check_your_skin_loader():
+    logger.info('Start Check_your_skin_tests_results import')
+    importer = tests_results_importer()
+    importer.connect(connection_string)
+    importer.get_tests_results_final()
+    importer.save_tests_results()
+    importer.disconnect()
+    logger.info('End Check_your_skin_tests_results import')
+
+
+if __name__ == '__main__':
+    cli()
